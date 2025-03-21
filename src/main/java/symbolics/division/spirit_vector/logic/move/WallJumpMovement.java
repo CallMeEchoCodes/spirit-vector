d9ef05b0486e77d5d8103eb0d21447390ecf3508
hama <hama@hama.industries>
2025-03-20 22:39:42 -0700

    add reminder for later

--- src/main/java/symbolics/division/spirit_vector/logic/move/WallJumpMovement.java
+++ src/main/java/symbolics/division/spirit_vector/logic/move/WallJumpMovement.java
@@ -8,129 +8,147 @@ import net.minecraft.world.World;
 import org.joml.Vector3f;
 import symbolics.division.spirit_vector.SpiritVectorMod;
 import symbolics.division.spirit_vector.logic.TravelMovementContext;
-import symbolics.division.spirit_vector.logic.vector.SpiritVector;
 import symbolics.division.spirit_vector.logic.input.Input;
 import symbolics.division.spirit_vector.logic.state.ManagedState;
+import symbolics.division.spirit_vector.logic.vector.SpiritVector;
 import symbolics.division.spirit_vector.logic.vector.VectorType;
 
 public class WallJumpMovement extends AbstractMovementType {
-    protected static final int MOMENTUM_GAINED = SpiritVector.MAX_MOMENTUM / 20;
-    protected static final float AXIS_ALIGN_THRESHOLD = -(float)Math.cos(Math.PI/4-0.01); // must be < cos(pi/4) or we can't consistently choose an inverted
-    protected static final Identifier WALL_JUMP_PLANE_TRACKER = SpiritVectorMod.id("wall_jump_plane_tracker");
+	protected static final int MOMENTUM_GAINED = SpiritVector.MAX_MOMENTUM / 20;
+	protected static final float AXIS_ALIGN_THRESHOLD = -(float) Math.cos(Math.PI / 4 - 0.01); // must be < cos(pi/4) or we can't consistently choose an inverted
+	protected static final Identifier WALL_JUMP_PLANE_TRACKER = SpiritVectorMod.id("wall_jump_plane_tracker");
 
-    private static class WallJumpPlaneTracker extends ManagedState {
-        public Pair<Direction, Integer> prevPlane;
-        public WallJumpPlaneTracker(SpiritVector sv) {
-            super(sv);
-        }
+	private static class WallJumpPlaneTracker extends ManagedState {
+		public Pair<Direction, Integer> prevPlane;
 
-        public boolean allowable(Direction dir, Vec3d pos) {
-            return prevPlane == null
-                    || dir != prevPlane.getLeft()
-                    || (int)pos.getComponentAlongAxis(dir.getAxis()) != prevPlane.getRight();
-        }
+		public WallJumpPlaneTracker(SpiritVector sv) {
+			super(sv);
+		}
 
-        public void set(Direction dir, int v) {
-            this.prevPlane = new Pair<>(dir, v);
-        }
+		public boolean allowable(Direction dir, Vec3d pos) {
+			return prevPlane == null
+				|| dir != prevPlane.getLeft()
+				|| (int) pos.getComponentAlongAxis(dir.getAxis()) != prevPlane.getRight();
+		}
 
-        public void clear() {
-            this.prevPlane = null;
-        }
-    }
+		public void set(Direction dir, int v) {
+			this.prevPlane = new Pair<>(dir, v);
+		}
 
-    public static void resetWallJumpPlane(SpiritVector sv) {
-        ((WallJumpPlaneTracker)sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER)).clear();
-    }
+		public void clear() {
+			this.prevPlane = null;
+		}
+	}
 
-    // convert context to input used for wall jumps
-    // normal if normally valid, and
-    // orthogonal (to wall) otherwise.
-    protected static Pair<Vec3d, Direction> getWalljumpingInput(SpiritVector sv, TravelMovementContext ctx) {
-        Vec3d input = MovementUtils.augmentedInput(sv, ctx);
-        Vector3f inputV3f = input.toVector3f();
-        Vec3d pos = sv.user.getPos().add(0, 0.5, 0);
+	public static void resetWallJumpPlane(SpiritVector sv) {
+		((WallJumpPlaneTracker) sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER)).clear();
+	}
 
-        World world = sv.user.getWorld();
-        WallJumpPlaneTracker planeState = (WallJumpPlaneTracker)sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER);
+	// convert context to input used for wall jumps
+	// normal if normally valid, and
+	// orthogonal (to wall) otherwise.
+	protected static Pair<Vec3d, Direction> getWalljumpingInput(SpiritVector sv, TravelMovementContext ctx) {
+		Vec3d input = MovementUtils.augmentedInput(sv, ctx);
+		Vector3f inputV3f = input.toVector3f();
+		Vec3d pos = sv.user.getPos().add(0, 0.5, 0);
 
-        Vec3d invertedInput = null;
-        Direction invertedDir = null;
+		World world = sv.user.getWorld();
+		WallJumpPlaneTracker planeState = (WallJumpPlaneTracker) sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER);
 
-        for (Direction dir : Direction.values()) {
-            // check if ok for a jump
-            if (dir == Direction.DOWN || dir == Direction.UP
-                    || !MovementUtils.validWallJumpAnchor(world, pos, dir)
-                    || !planeState.allowable(dir, pos)
-            ) continue;
+		Vec3d invertedInput = null;
+		Direction invertedDir = null;
 
-            // determine if return normal input, or prepare to return inverted
-            var normal = dir.getOpposite().getUnitVector();
-            float dp = normal.dot(inputV3f);
-            if (dp > 0) {
-                return new Pair<>(input, dir);
-            } else if(dp < AXIS_ALIGN_THRESHOLD) {
-                invertedInput = new Vec3d(normal);
-                invertedDir = dir;
-            }
-        }
+		for (Direction dir : Direction.values()) {
+			// check if ok for a jump
+			if (dir == Direction.DOWN || dir == Direction.UP
+				|| !MovementUtils.validWallJumpAnchor(world, pos, dir)
+				|| !planeState.allowable(dir, pos)
+			) continue;
 
-        if (invertedInput != null) {
-            return new Pair<>(invertedInput, invertedDir);
-        } else {
-            return null;
-        }
-    }
+			// determine if return normal input, or prepare to return inverted
+			var normal = dir.getOpposite().getUnitVector();
+			float dp = normal.dot(inputV3f);
+			if (dp > 0) { // jump away
+				return new Pair<>(input, dir);
+			} else if (dp < AXIS_ALIGN_THRESHOLD) { // jump opposite
+				invertedInput = new Vec3d(normal);
+				invertedDir = dir;
+			}
+		}
 
-    public WallJumpMovement(Identifier id) {
-        super(id);
-    }
+		if (invertedInput != null) {
+			return new Pair<>(invertedInput, invertedDir);
+		} else {
+			/* REMINDER TO FUTURE SELF DO NOT DO THIS */
+//			if (invertedInput != null) {
+//				return new Pair<>(invertedInput, invertedDir);
+//			} else { // jumping along wall
+//				Direction jumpDirection = Direction.getFacing(input);
+//				Direction right = jumpDirection.rotateYClockwise();
+//				Direction left = jumpDirection.rotateYCounterclockwise();
+//				boolean hasRight = planeState.allowable(right, pos) && MovementUtils.validWallJumpAnchor(world, pos, right);
+//				boolean hasLeft = planeState.allowable(left, pos) && MovementUtils.validWallJumpAnchor(world, pos, left);
+//				Direction result = hasLeft && hasRight && planeState.allowable(jumpDirection, pos) ? jumpDirection // on either side, pretend a wall behind us
+//					: hasLeft ? left // or else jumping along
+//					: hasRight ? right
+//					: null;
+//				if (result != null) {
+//					return new Pair<>(new Vec3d(jumpDirection.getUnitVector()), result);
+//				}
+//			}
+			return null;
+		}
+	}
 
-    @Override
-    public void configure(SpiritVector sv) {
-        sv.stateManager().register(WALL_JUMP_PLANE_TRACKER, new WallJumpPlaneTracker(sv));
-    }
+	public WallJumpMovement(Identifier id) {
+		super(id);
+	}
 
-    @Override
-    public boolean testMovementCondition(SpiritVector sv, TravelMovementContext ctx) {
-        return !sv.user.isOnGround()
-                && MovementUtils.idealWalljumpingConditions(sv, ctx)
-                && sv.inputManager().consume(Input.JUMP);
-    }
+	@Override
+	public void configure(SpiritVector sv) {
+		sv.stateManager().register(WALL_JUMP_PLANE_TRACKER, new WallJumpPlaneTracker(sv));
+	}
 
-    @Override
-    public void travel(SpiritVector sv, TravelMovementContext ctx) {
-        var result = getWalljumpingInput(sv, ctx);
-        if (result == null) { // input invalid
-            NEUTRAL.travel(sv, ctx);
-            return;
-        }
+	@Override
+	public boolean testMovementCondition(SpiritVector sv, TravelMovementContext ctx) {
+		return !sv.user.isOnGround()
+			&& MovementUtils.idealWalljumpingConditions(sv, ctx)
+			&& sv.inputManager().consume(Input.JUMP);
+	}
 
-        Direction dir = result.getRight();
-        Vec3d input = result.getLeft();
+	@Override
+	public void travel(SpiritVector sv, TravelMovementContext ctx) {
+		var result = getWalljumpingInput(sv, ctx);
+		if (result == null) { // input invalid
+			NEUTRAL.travel(sv, ctx);
+			return;
+		}
 
-        Vec3d motion = new Vec3d(input.x/2, 0.5, input.z/2);
-        if (sv.getMoveState() == this) { // only apply for normal walljumps
-            motion = motion.multiply(sv.consumeSpeedMultiplier());
-        }
+		Direction dir = result.getRight();
+		Vec3d input = result.getLeft();
 
-        if (sv.is(VectorType.DREAM)) {
-            sv.user.addVelocity(motion);
-        } else {
-            sv.user.setVelocity(motion);
-        }
+		Vec3d motion = new Vec3d(input.x / 2, 0.5, input.z / 2);
+		if (sv.getMoveState() == this) { // only apply for normal walljumps
+			motion = motion.multiply(sv.consumeSpeedMultiplier());
+		}
 
-        ((WallJumpPlaneTracker)sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER)).set(
-                dir, (int)sv.user.getPos().getComponentAlongAxis(dir.getAxis())
-        );
-        sv.effectsManager().spawnRing(sv.user.getPos(), motion);
-    }
+		if (sv.is(VectorType.DREAM)) {
+			sv.user.addVelocity(motion);
+		} else {
+			sv.user.setVelocity(motion);
+		}
 
-    @Override
-    public void updateValues(SpiritVector sv) {
-        if (!sv.is(VectorType.DREAM)) {
-            sv.modifyMomentum(MOMENTUM_GAINED);
-            sv.stateManager().enableStateFor(SpiritVector.MOMENTUM_DECAY_GRACE_STATE, 20);
-        }
-    }
+		((WallJumpPlaneTracker) sv.stateManager().getState(WALL_JUMP_PLANE_TRACKER)).set(
+			dir, (int) sv.user.getPos().getComponentAlongAxis(dir.getAxis())
+		);
+		sv.effectsManager().spawnRing(sv.user.getPos(), motion);
+	}
+
+	@Override
+	public void updateValues(SpiritVector sv) {
+		if (!sv.is(VectorType.DREAM)) {
+			sv.modifyMomentum(MOMENTUM_GAINED);
+			sv.stateManager().enableStateFor(SpiritVector.MOMENTUM_DECAY_GRACE_STATE, 20);
+		}
+	}
 }
